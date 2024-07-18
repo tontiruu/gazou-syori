64666f49e5645139ca8736c9f8f2659db4a5ddb1
tontiruu <113325721+tontiruu@users.noreply.github.com>
2024-07-19 00:31:06 +0900

    銀で作った銀閣寺

--- image_processing/util.py
+++ image_processing/util.py
@@ -1,18 +1,83 @@
 import glob
-from PIL import Image
 import cv2
+
+import numpy as np
+from pydantic import BaseModel
+from datetime import datetime
+import tqdm
 
 
 def resize_image_in_dir(path: str, size: int = 20) -> None:
     img_paths = glob.glob(f"{path}/*.jpg")
-    for index, img_path in enumerate(img_paths):
+    for img_path in img_paths:
         img = cv2.imread(img_path)
         img = cv2.resize(img, (size, size))
-        cv2.imwrite(f"{path}/{index}.jpg", img)
+        cv2.imwrite(img_path, img)
     print("resize Done")
 
 
-def get_materials_mean_color(path: str = "color_materials") -> None:
+class MaterialMeanColor(BaseModel):
+    means: list
+    imgs: list
+
+
+def parse_materials_mean_color(path: str = "color_materials") -> MaterialMeanColor:
+    means: list[np.ndarray] = []
+    imgs: list[np.ndarray] = []
+
     img_paths = glob.glob(f"{path}/*.jpg")
-    for index, img_path in enumerate(img_paths):
-        img = cv2.imread(img_path)
+
+    for img_path in img_paths:
+        color_sum = np.array([0, 0, 0])
+        img: np.ndarray = cv2.imread(img_path)
+        for row in img:
+            for pixcel in row:
+                color_sum += pixcel
+        color_mean = color_sum / (len(img) * len(img[0]))
+        means.append(color_mean)
+        imgs.append(img)
+
+    return MaterialMeanColor(means=means, imgs=imgs)
+
+
+def calculate_color_diff(color_1: np.ndarray, color_2: np.ndarray) -> int:
+    return sum(abs(color_1 - color_2))
+
+
+def search_fit_color(color: np.ndarray, color_palette: list[np.ndarray]) -> int:
+    min_color_diff = 99999999
+    result_index = 0
+    for index, cp in enumerate(color_palette):
+        color_diff = calculate_color_diff(color, cp)
+        if color_diff < min_color_diff:
+            min_color_diff = color_diff
+            result_index = index
+    return result_index
+
+
+def create_mosaic_image(
+    origin_img_path: str, color_palette: MaterialMeanColor, resize_rate: float = 0.4
+):
+    output_img = None
+
+    origin_img = cv2.imread(origin_img_path)
+    origin_img = cv2.resize(origin_img, None, fx=resize_rate, fy=resize_rate)
+
+    for row in tqdm.tqdm(origin_img):
+        output_img_row = None
+        for pixel in row:
+            use_palette_index = search_fit_color(pixel, color_palette.means)
+            if output_img_row is None:
+                output_img_row = color_palette.imgs[use_palette_index]
+            else:
+                output_img_row = np.concat(
+                    [output_img_row, color_palette.imgs[use_palette_index]], axis=1
+                )
+        if output_img is None:
+            output_img = output_img_row
+        else:
+            output_img = np.concat([output_img, output_img_row], axis=0)
+
+    now = datetime.now()
+    timestamp = now.strftime("%Y-%m-%d %H:%M:%S %Z%z")
+    cv2.imwrite(f"./output/{timestamp}.jpg", output_img)
---
+++ main_process.py
@@ -0,0 +1,25 @@
+import cv2
+from scryping.image_crawler import scrape_img
+from image_processing import util
+
+
+def create_image(
+    material_word: str,
+    target_word: str,
+    material_max_nums: int = 40,
+    material_img_size: int = 40,
+    target_resize_rate: float = 0.1,
+) -> None:
+    print(f"{material_word}でできた、{target_word}を生成します")
+    scrape_img(keyword=material_word, max_num=material_max_nums, path="color_materials")
+    scrape_img(keyword=target_word, max_num=1, path="convert_images")
+    print("\n\n Image collection is complete! \n\n")
+    util.resize_image_in_dir("color_materials", size=material_img_size)
+    print("\n\nGenerating image ...")
+    color_palette: util.MaterialMeanColor = util.parse_materials_mean_color()
+    util.create_mosaic_image(
+        "convert_images/000001.jpg",
+        color_palette=color_palette,
+        resize_rate=target_resize_rate,
+    )
+    print("\n\nDone!")
